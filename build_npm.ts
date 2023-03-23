7ce41d815fea9f7854994f32cf70154fb5585dd4
Muthu Kumar <muthukumar@thefeathers.in>
2023-03-24 01:30:38 +0530

    ci: just use GITHUB_REF in build_npm.ts

--- build_npm.ts
+++ build_npm.ts
@@ -1,7 +1,8 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
 import pkg from "./package.json" assert { type: "json" };
 
-const version = Deno.env.get("VERSION");
+const tag = Deno.env.get("GITHUB_REF");
+const version = tag ? tag.slice(11) : Deno.env.get("VERSION");
 
 if (!version)
 	throw new Error("VERSION environment variable not found. Use similar to `VERSION=0.1.0 deno run -A build_npm.ts`");
